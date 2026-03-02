d120cc7beeb4260135d4b9559bddf5183a65dec2
sanjay3261-wed <nallamuthusanjay4387@gmail.com>
2026-03-02 17:38:10 +0530

    Add files via upload

---
+++ p 18.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for(int i=1;i<=n;i++) {
+        for(int j=1;j<=i;j++)
+            printf("*");
+        for(int s=1;s<=2*(n-i);s++)
+            printf(" ");
+        for(int j=1;j<=i;j++)
+            printf("*");
+        printf("\n");
+    }
+    for(int i=n-1;i>=1;i--) {
+        for(int j=1;j<=i;j++)
+            printf("*");
+        for(int s=1;s<=2*(n-i);s++)
+            printf(" ");
+        for(int j=1;j<=i;j++)
+            printf("*");
+        printf("\n");
+    }
+    return 0;
+}
